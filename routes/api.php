ffe959c5574e8f516cca933d1ddeae01b4ead090
Luis <luismartinezmilla@gmail.com>
2022-02-24 14:22:06 +0100

    as

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\imagenController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::put('/subirImagen', [InkmeController::class,'subirImagen']);
-Route::put('/descargarImagen', [InkmeController::class,'descargarImagen']);
+Route::put('/subirImagen', [imagenController::class,'subirImagen']);
+Route::put('/descargarImagen', [imagenController::class,'descargarImagen']);
