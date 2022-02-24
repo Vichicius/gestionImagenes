720ab5d059952d13c36ae0a599cdf8ab21b6f2a7
Luis <luismartinezmilla@gmail.com>
2022-02-24 12:24:33 +0100

    subir y descargar v1

--- app/Http/Controllers/imagenController.php
+++ app/Http/Controllers/imagenController.php
@@ -2,21 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ejemplo;
 use Exception;
 use Illuminate\Http\Request;
 
 class imagenController extends Controller
 {
-    public function subirImagen(Request $req){ //Pide: user_id, api_token (opcional para saber que es un tatuador) || Devuelve: "status" "msg" y "[{post}]"
+    public function subirImagen(Request $req){ //Pide: imagen || Devuelve: "status" "msg" y "[{post}]"
         $jdata = $req->getContent();
         $data = json_decode($jdata);
 
         $response["status"]=1;
         try{
-            if(isset($data->user_id) && isset($data->api_token)){
-
+            if(isset($data->imagen)){
+                $imagen = $data->imagen;
+                $data->imagen->file('archivoInput')->storeAs('archivos', $imagen->getClientOriginalName());
+                $ejemplo = new ejemplo();
+                $ejemplo->photo = "desarrolladorapp.com/inkme/public/archivos/".$imagen->getClientOriginalName();
+                $ejemplo->save();
+                $response["photo"] = $ejemplo;
             }else{
-                throw new Exception("Error: Introduce user_id y api_token (aunque esté vacio)");
+                throw new Exception("Error: Introduce imagen");
             }
         }catch(\Exception $e){
             $response["status"]=0;
@@ -25,16 +31,16 @@ class imagenController extends Controller
         return response()->json($response);
     }
 
-    public function descargarImagen(Request $req){ //Pide: user_id, api_token (opcional para saber que es un tatuador) || Devuelve: "status" "msg" y "[{post}]"
+    public function descargarImagen(Request $req){ //Pide: id || Devuelve: "status" "msg" y "[{post}]"
         $jdata = $req->getContent();
         $data = json_decode($jdata);
 
         $response["status"]=1;
         try{
-            if(isset($data->user_id) && isset($data->api_token)){
-
+            if(isset($data->id)){
+                $response['photo'] = ejemplo::find($data->id);
             }else{
-                throw new Exception("Error: Introduce user_id y api_token (aunque esté vacio)");
+                throw new Exception("Error: Introduce id y api_token (aunque esté vacio)");
             }
         }catch(\Exception $e){
             $response["status"]=0;
